81128a5e9d092d8571b3ba7271b9d60e6ea80cde
Suman Rana <sumanrana2062@gmail.com>
2026-03-31 09:19:53 +0545

    cleaned branch

--- program.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <sys/types.h>
-void callFor();
-int main()
-{
-    fork();
-    printf("Hello World");
-    return 0;
-}
--- tempCodeRunnerFile.c
+++
@@ -1,5 +0,0 @@
-  // if(i==0){
-        //     nearestDistance=distance;
-        //     nearestIndex=i;
-        //     continue;
-        // }
